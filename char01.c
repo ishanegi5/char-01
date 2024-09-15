e85aa7cfc940134d9b82d04fd38f7f9b44d87c91
Isha Negi <itsishanegi@gmail.com>
2024-09-15 21:03:55 +0530

    Add files via upload

---
+++ char01.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+#define MAX 10
+
+void main()
+
+{ char a[MAX]= {'a','d','f'};
+
+for(int j=0;j<MAX;j++)
+{
+    printf("Enter %d char: ",j+1);
+  
+scanf(" %c",&a[j]);
+
+}
+   
+   
+for(int i=0;i<MAX;i++)
+ {
+       printf("%d. %c\n",i+1,a[i]);
+  
+ }
+
+}
